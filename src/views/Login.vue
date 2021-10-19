addbc752e3632bc88862a48226d1a768a6cfcd87
fanfan <772205876@qq.com>
2021-10-19 15:12:04 +0800

    ff2021/10/19-优化注册以及页面跳转

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,27 +3,144 @@
 		<van-tabs v-model="activeName">
 			<van-tab title="Sign Up" name="SignUp">
 				<div class="inputBox">
-					<div class="main_content" :style="{maxWidth: pageWidth+'px'}">
+					<!-- <div class="main_content" :style="{maxWidth: pageWidth+'px'}">
 						<Common :pageData='pageContent'></Common>
-					</div>
-					<!-- <div class="box">
-						<van-field v-model="registForm.name" autocomplete="off" placeholder="name*" />
-						<van-field v-model="registForm.phone" autocomplete="off" placeholder="Phone*" />
-						<van-field v-model="registForm.password" autocomplete="off" type="password" placeholder="Password*" />
-						<van-field v-model="registForm.repassword" autocomplete="off" type="password" placeholder="Repeat Password*" />
+					</div> -->
+
+					<van-form validate-first @failed="onFailed" @submit="verifyCode">
 						<van-field
-						  style="padding-right: 0"
-						  v-model="registForm.smscode"
-						  center
-						  clearable
-						  placeholder="Enter Verification Code"
-						>
-						  <template #button>
-						    <van-button size="small" class="submitBtn" :disabled='isSms' @click="sendCode();">Send SMS Code</van-button>
-						  </template>
-						</van-field>
-					</div>
-					<van-button class="loginBtn" type="default" @click="register();">Sign Up</van-button> -->
+                            v-model="registForm.name"
+                            name="Full Name / Company Name"
+                            center
+                            :required="true"
+                            type="text"
+                            label="Full Name / Company Name"
+                            placeholder="Full Name / Company Name"
+                            :rules="[{ required: true, message: '请输入Full Name / Company Name' }]"
+                          />
+						<van-field
+                            v-model="registForm.passport"
+                            name="NRIC Name or Passport # / Company Re.Number"
+                            center
+                            :required="true"
+                            type="text"
+                            label="NRIC Name or Passport # / Company Re.Number"
+                            placeholder="NRIC Name or Passport # / Company Re.Number"
+                            :rules="[{ required: true, message: '请输入NRIC Name or Passport # / Company Re.Number' }]"
+                          />
+						<van-field
+                            v-model="registForm.phone"
+                            name="Mobile Number"
+                            center
+                            :required="true"
+                            type="text"
+                            label="Mobile Number"
+                            placeholder="Mobile Number"
+                            :rules="[{ required: true, message: '请输入Mobile Number' }]"
+                          />
+                        <van-field
+                            v-model="phoneList.verify_code"
+                            center
+                            :required="true"
+                            label="验证码"
+                            placeholder="请输入短信验证码"
+                            :rules="[{required: true, message: '请输入验证码'}]"
+                            >
+                            <van-button class="SMSconfirm" slot="button" native-type="button" :disabled='phoneList.isSms' @click="sendCode();">发送验证码</van-button>
+                        </van-field>
+						<van-field
+                            v-model="registForm.address"
+                            name="Residential Address"
+                            center
+                            :required="true"
+                            type="text"
+                            label="Residential Address"
+                            placeholder="Residential Address"
+                            :rules="[{ required: true, message: '请输入Residential Address' }]"
+                          />
+						<van-field
+                            v-model="registForm.birthday"
+                            name="Date of Birth"
+                            center
+                            :required="true"
+                            type="text"
+                            label="Date of Birth"
+                            placeholder="YYYY-MM-DD"
+                            @click="onShowPicker()"
+                          />
+                        <van-popup v-model="isShowPicker" position="bottom">
+			                <van-datetime-picker
+			                    v-model="currentContent"
+			                    type="date"
+			                    :min-date="minDate"
+			                    :max-date="maxDate"
+			                    @cancel="onHiddenPicker"
+			                    @confirm="onConfirmPicker"
+			                />
+			            </van-popup>
+						<van-field
+                            v-model="registForm.sponsor"
+                            name="Introducer ID"
+                            center
+                            :required="true"
+                            type="text"
+                            label="Introducer ID"
+                            placeholder="Introducer ID"
+                            :rules="[{ required: true, message: '请输入Introducer ID' }]"
+                          />
+						<van-field
+                            v-model="registForm.bank"
+                            name="Bank Name"
+                            center
+                            :required="true"
+                            type="text"
+                            label="Bank Name"
+                            placeholder="Bank Name"
+                            :rules="[{ required: true, message: '请输入Bank Name' }]"
+                          />
+						<van-field
+                            v-model="registForm.bankAccountNo"
+                            name="Bank Account No."
+                            center
+                            :required="true"
+                            type="text"
+                            label="Bank Account No."
+                            placeholder="Bank Account No."
+                            :rules="[{ required: true, message: '请输入Bank Account No.' }]"
+                          />
+						<van-field
+                            v-model="registForm.bankAccountName"
+                            name="Bank Account Name"
+                            center
+                            :required="true"
+                            type="text"
+                            label="Bank Account Name"
+                            placeholder="Bank Account Name"
+                            :rules="[{ required: true, message: '请输入Bank Account Name' }]"
+                          />
+                        <van-field
+                            v-model="registForm.password"
+                            name="密码"
+                            center
+                            :required="true"
+                            type="password"
+                            label="密码"
+                            placeholder="密码"
+                            :rules="[{ required: true, message: '请输入密码' }]"
+                          />
+                        <van-field
+                            v-model="registForm.password_confirmation"
+                            name="确认密码"
+                            center
+                            :required="true"
+                            type="password"
+                            label="确认密码"
+                            placeholder="确认密码"
+                            :rules="[{ required: true, message: '请再次输入密码' }]"
+                          />
+                    	<van-button class="loginBtn" type="default" native-type="submit">Sign Up</van-button>
+					</van-form>
+				
 				</div>
 			</van-tab>
 			<van-tab title="Sign In" name="SignIn">
@@ -58,11 +175,24 @@
 				},
 				registForm:{
 					name:'',
-					phone:'',
 					password:'',
-					repassword:'',
-					smskey: '',
-					smscode:'',
+					phone:'',
+					passport:'',
+					address:'',
+					birthday:'',
+					sponsor:'',
+					bank:'',
+					bankAccountNo:'',
+					bankAccountName:'',
+					password_confirmation:'',
+				},
+				isShowPicker: false,
+            	currentContent: new Date(),
+				minDate:new Date(1950, 0, 1),
+            	maxDate:new Date(),
+				phoneList: {
+					verify_code: '',
+					isSms: false,
 				},
 			}
 		},
@@ -131,77 +261,151 @@
 					this.$toast('请输入用户名和密码');
 				}
 			},
+			// 获取验证码
 			sendCode(){
-				if(this.registForm.phone){
-					this.$axios({
-		                method: 'post',
-		                url: '/api/v1/verificationCodes',
-		                data:{
-		                	phone:this.registForm.phone,
-		                },
-		            }).then(res => {
-		            	console.log(res);
-		            	this.isSms=true;
-		            	this.registForm.smskey=res.key;
-		            	const vm=this;
-		            	setTimeout(function(){
-		            		vm.isSms=false;
-		            	},60000);
-		            }).catch(err => {
-		            	this.$toast({
-		            		type:'fail',
-		            		message:'获取验证码失败',
-	            		});
-		            });
-				}else{
-					this.$toast('请输入手机号');
-				}
+				if(!this.phoneList.isSms){
+	                if(this.registForm.phone){
+	                    var data = [{"phone":this.registForm.phone}]
+	                    this.$axios({
+	                        method: 'POST',
+	                        url: '/api/v1/sin_up/sms/verify_code',
+	                        data:{
+	                            phone: JSON.stringify(data),
+	                        },
+	                    }).then(res => {
+	                        console.log(res);
+	                        if(res.state_code==200){
+	                            this.$toast({
+	                                type:'success',
+	                                message:res.message,
+	                            });
+	                            this.phoneList.isSms=true;
+	                            const vm=this;
+	                            setTimeout(function(){
+	                                vm.phoneList.isSms=false;
+	                            },60000);
+	                        }else{
+	                            this.$toast({
+	                                type:'fail',
+	                                message:res.message,
+	                            });
+	                        }
+	                    }).catch(err => {
+	                        this.$toast({
+	                            type:'fail',
+	                            message:'获取验证码失败',
+	                        });
+	                    });
+	                }else{
+	                    this.$toast('请先输入手机号');
+	                }
+	            }
 			},
+			// 展示时间或日期弹框
+	        onShowPicker(){
+	            this.isShowPicker=true;
+	        },
+	        onHiddenPicker(){
+	            this.currentContent = '';
+	            this.isShowPicker=false;
+	        },
+	        onConfirmPicker(){
+	            this.registForm.birthday = this.formatDateYMD(this.currentContent);
+	            this.isShowPicker = false;
+	        },
+	        formatDateYMD(value){
+	            if (!value){ 
+	                return '';
+	            }else{
+	                var date = new Date(value);
+	                var Y = date.getFullYear()+'-';
+	                var M = (date.getMonth()+1)+ '-';
+	                var D = date.getDate();
+	                return Y+M+D;
+	            }
+	        },
+	        onFailed(values,errorInfo) {
+	            console.log('failed', errorInfo);
+	            for(var key in values){
+	                console.log(key,values[key]);
+	            }
+	        },
+	        // 验证验证码
+	        verifyCode(values){
+	            let data = [];
+	            var phoneInfo = {
+                    phone: this.registForm.phone,
+                    verify_code: this.phoneList.verify_code,
+                };
+                data.push(phoneInfo);
+	            this.$axios({
+	                method: 'GET',
+	                url: '/api/v1/sin_up/sms/verify_code?data='+JSON.stringify(data),
+	            }).then(res => {
+	                console.log(res);
+	                if(res.state_code==200){
+	                    const vm=this;
+	                    setTimeout(function(){
+	                        vm.register();
+	                        // vm.register(values);
+	                    },600);
+	                }else{
+	                    this.$toast({
+	                        type:'fail',
+	                        message:res.message,
+	                    });
+	                }
+	            }).catch(err => {
+	                this.$toast({
+	                    type:'fail',
+	                    message:'验证码错误',
+	                });
+	            });
+	        },
 			register(){
-				if(!this.registForm.smskey){
-					this.$toast('请先获取验证码');
-					return;
-				}
-				if(this.registForm.password!=this.registForm.repassword){
+				if(this.registForm.password!=this.registForm.password_confirmation){
 					this.$toast('两次密码不一致');
 					return;
 				}
-				if(this.registForm.name&&this.registForm.password&&this.registForm.smscode){
-					this.$axios({
-						method: 'post',
-						url: '/api/v1/users',
-						data:{
-							name:this.registForm.name,
-							password:this.registForm.password,
-							phone: this.registForm.phone,
-							verification_key: this.registForm.smskey,
-							verification_code: this.registForm.smscode,
-						},
-					}).then(res => {
-						console.log(res);
-						if(res.id){
-							this.$toast({
-								type:'success',
-								message:'注册成功，请前去登录',
-							});          	
-							const vm=this;
-							setTimeout(function(){
-								// vm.$router.push('/');
-								vm.activeName = 'SignIn';
-							},1000);
-						}
-					}).catch(err => {
-						this.$toast.allowMultiple();
-						for (var item in err.errors) {
-							this.$toast({
-								type:'fail',
-								message:err.errors[item],
-							});
-						}
-					});
-				}else{
-					this.$toast('请输入完全部信息');
-				}
+				console.log(this.registForm);
+				// this.$axios({
+				// 	method: 'post',
+				// 	url: '/api/v1/users',
+				// 	data:{
+				// 		name:this.registForm.name,
+				// 		password:this.registForm.password,
+				// 		phone:this.registForm.phone,
+				// 		passport:this.registForm.passport,
+				// 		address:this.registForm.address,
+				// 		birthday:this.registForm.birthday,
+				// 		sponsor:this.registForm.sponsor,
+				// 		bank:this.registForm.bank,
+				// 		bankAccountNo:this.registForm.bankAccountNo,
+				// 		bankAccountName:this.registForm.bankAccountName,
+				// 		password_confirmation:this.registForm.password_confirmation,
+				// 	},
+				// }).then(res => {
+				// 	console.log(res);
+				// 	if(res.id){
+				// 		this.$toast({
+				// 			type:'success',
+				// 			message:'注册成功，请前去登录',
+				// 		});          	
+				// 		const vm=this;
+				// 		setTimeout(function(){
+				// 			// vm.$router.push('/');
+				// 			vm.activeName = 'SignIn';
+				// 		},1000);
+				// 	}
+				// }).catch(err => {
+				// 	this.$toast.allowMultiple();
+				// 	for (var item in err.errors) {
+				// 		this.$toast({
+				// 			type:'fail',
+				// 			message:err.errors[item],
+				// 		});
+				// 	}
+				// });
 			},
 		}
 	}
@@ -228,6 +432,14 @@
 	color: #fff;
 	border-color: #87898A;
 	border-radius: 46px;
+}
+.SMSconfirm{
+    color: #fff;
+    border: none;
+    outline: none;
+    background-color: #2f75f4;
+    font-size: 16px;
+    border-radius: 13px;
 }
 @media screen and (max-width: 576px){
   .login{margin-top: 0;}
