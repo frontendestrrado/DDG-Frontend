761a6cbf61403026425dd5a8a39abf451cde7bf4
JHON-dd <369763019@qq.com>
2022-01-06 18:03:51 +0800

    feat:

--- src/views/OurServices.vue
+++ src/views/OurServices.vue
@@ -1,5 +1,179 @@
 <template>
-  <div>
-    Our Services
+  <div class="ourServices">
+      <h1>Our Services</h1>
+    <div class="ourServicesOne">
+      
+    </div>
+    <div class="ourServicesOneContent">
+        <p>
+            When a person is deceased.ALL of the person’ s assets will be frozen!
+            DDG helps address the estate distribution to be according to a person’s wishes in
+            case of death,being incapacitated,or comatose.
+            DDG’s Advisor creates a master plan for the management of your property during life
+            and the distribution of that property at the person’s death.
+      </p>
+      <h2>Estate Pianning:</h2>
+      <ol>
+          <li> 
+              Give more control over assets during Iife
+          </li>
+          <li>
+              Provide care when disabied
+          </li>
+          <li>
+             Allow for tho trensfor of woaith to whom and whon wanted at the lowest possible cost
+          </li>
+      </ol>
+      <div>
+        <label>Common estate planning issues addressed in the weaith management process include: </label>
+        <van-checkbox-group v-model="result" >
+            <van-checkbox name="a">Transfer of wealth</van-checkbox>
+            <van-checkbox name="b">Minimization of transfer taxes</van-checkbox>
+            <van-checkbox name="b">Asset protection</van-checkbox>
+            <van-checkbox name="b">Charitable contribution</van-checkbox>
+            <van-checkbox name="b">Confdentiality without disciosure at court</van-checkbox>
+        </van-checkbox-group>
+      </div>
+    </div>
+    <div class="ourServicesTwo">
+      
+    </div>
+    <div class="ourServicesThree">
+        <span class="enquiries">For more enquiries</span>
+        <van-button @click="toContactUs" type="default" color="7C655D" class="contactUs">Contact Us</van-button>
+    </div>
   </div>
 </template>
+
+<script>
+import Common from "@/components/mode/common.vue";
+export default {
+  components: {
+    Common,
+  },
+  name: "OurServices",
+  data() {
+    return {
+      result: ['a', 'b'],
+      pageContent: [],
+      pageWidth: "1200",
+    };
+  },
+  mounted() {
+    // this.getourServicesPage();
+  },
+  methods: {
+    // getourServicesPage() {
+    //   this.$axios({
+    //     method: "get",
+    //     url: "/api/v1/pages?type=1",
+    //   })
+    //     .then((res) => {
+    //       console.log(res);
+    //       if (res.status == 200) {
+    //         this.pageContent = res.data.modules;
+    //         this.pageWidth = res.data.width;
+    //         this.pageContent.forEach((item, i) => {
+    //           if (item.type == 9) {
+    //             item.data_collects.forEach((itemi, j) => {
+    //               this.$set(
+    //                 this.pageContent[i].data_collects[j],
+    //                 "content",
+    //                 ""
+    //               );
+    //             });
+    //           }
+    //         });
+    //       } else {
+    //         this.$router.push("/");
+    //       }
+    //     })
+    //     .catch((error) => {});
+    // },
+    toContactUs() {
+      this.$store.commit('changePage', {
+        tabbar: '/ContactUs',
+        title: 'Contact Us',
+      });
+      this.$router.push('/ContactUs');
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h1 {
+  font-weight: normal;
+  font-size: 3rem;
+  color: #7C655D;
+  margin-bottom:4rem;
+}
+a {
+  color: #42b983;
+}
+.ourServices{
+    .ourServicesOneContent{
+        padding:5rem 10rem;
+        color: #7C655D;
+        line-height: 3rem;
+        text-align: left;
+        p{
+            // letter-spacing: 0.1rem;
+            line-height: 3rem;
+            text-align: center;
+        }
+        h2{
+            margin-top: 4rem;
+        }
+        ol{
+            margin-bottom: 4rem;
+        }
+    }
+    .ourServicesOne{
+        background: url(../assets/img/OurServices/OurServices1.jpg);
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 50rem;
+        text-align: right;
+        position: relative;
+    }
+    .ourServicesTwo{
+        background: url(../assets/img/OurServices/OurServices2.jpg);
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 30rem;
+        text-align: right;
+        position: relative;
+    }
+    .ourServicesThree{
+        background: url(../assets/img/OurServices/OurServices3.jpg);
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 10rem;
+        text-align: right;
+        position: relative;
+        .enquiries{
+            font-size: 2rem;
+            color: #fff;
+            position: absolute;
+            top: 33%;
+            left: 30%;
+        }
+        .contactUs {
+            background-color: #fff;
+            position: absolute;
+            top: 25%;
+            left: 52%;
+            border-radius: 1rem;
+            height: 3rem;
+            width: 11rem;
+            span{
+              color: #7C655D;
+            }
+        }
+    }
+}
+</style>
--- src/views/TheAIGenerationt.vue
+++ src/views/TheAIGenerationt.vue
@@ -1,5 +1,145 @@
 <template>
-  <div>
-    The AI “爱” Generation Trust
+  <div class="Generation">
+      <h1>The AI“爱”Generation Trust</h1>
+    <div class="GenerationOne">
+        <h1>The AI  <span>“爱”</span></h1>
+        <h1>Generation Trust</h1>
+        <div class="content">
+            <p>
+              Al(means fovo in Mandarin) is at the root of our pbiiasopm
+              -this AI Genemtion trust,
+              under the purview of Asia indemationnl Trust Berhad nd diatributed excluniwely by DDG,
+              offers imdeccsble protection for yout assets due to the
+              indopendeint trustoo's mardate owur the trust asbets and distribution
+            </p>
+            <p style="margin-top:2rem">
+              A famlly's suoooss in sunthining tto wealth from one cenerstion to onother is ihentfnble.
+              end if reotiees careful ianning sne inmalermentation
+            </p>
+        </div> 
+    </div>
+    <div class="GenerationTwo">
+      
+    </div>
+    <div class="GenerationFour">
+      
+    </div>
+    <div class="GenerationThree">
+        <span class="enquiries">For more enquiries</span>
+        <van-button @click="toContactUs" type="default" color="7C655D" class="contactUs">Contact Us</van-button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+  },
+  name: "Generation",
+  data() {
+    return {
+      pageContent: [],
+      pageWidth: "1200",
+    };
+  },
+  mounted() {
+    // this.getGenerationPage();
+  },
+  methods: {
+    toContactUs() {
+      this.$store.commit('changePage', {
+        tabbar: '/ContactUs',
+        title: 'Contact Us',
+      });
+      this.$router.push('/ContactUs');
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h1 {
+  font-weight: normal;
+  font-size: 3rem;
+  color: #7C655D;
+  margin-bottom:4rem;
+}
+a {
+  color: #42b983;
+}
+.Generation{
+     color: #7C655D;
+     position: relative;
+    .GenerationOne{
+        background: url(../assets/img/Generation/AI1.jpg);
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 50rem;
+        text-align: center;
+        position: relative;
+        padding: 4rem;
+        h1{
+           margin-bottom:1rem;
+           span{
+             font-size: 4rem;
+           }
+        }
+        .content{
+           font-size: 2rem;
+           width: 36rem;
+           text-align: left;
+          //  line-height: 2rem;
+           position: absolute;
+           right: 0;
+           top:20rem;
+        }
+    }
+    .GenerationTwo{
+        background: url(../assets/img/Generation/AI2.jpg);
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 30rem;
+        text-align: right;
+        position: relative;
+    }
+    .GenerationFour{
+        background: url(../assets/img/Generation/AI3.jpg);
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 50rem;
+        text-align: right;
+        position: relative;
+    }
+    .GenerationThree{
+        background: url(../assets/img/Generation/AI4.jpg);
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        height: 10rem;
+        text-align: right;
+        position: relative;
+        .enquiries{
+            font-size: 2rem;
+            color: #fff;
+            position: absolute;
+            top: 33%;
+            left: 30%;
+        }
+        .contactUs {
+            background-color: #fff;
+            position: absolute;
+            top: 25%;
+            left: 52%;
+            border-radius: 1rem;
+            height: 3rem;
+            width: 11rem;
+            span{
+              color: #7C655D;
+            }
+        }
+    }
+}
+</style>
